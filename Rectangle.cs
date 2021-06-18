1a45f16eff17fe48231bdfb53d282f7bae16a20b
Dan <dannieabraham@gmail.com>
2021-06-18 17:13:45 +0530

    completed Rectangle class

--- Rectangle.cs
+++ Rectangle.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Assignment01
 {
-    class Class1
+    public class Rectangle
     {
+        private int length, breadth;
+
+        Rectangle()
+        {
+            length = 1;
+            breadth = 1;
+        }
+        Rectangle( int length, int breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+
+        public int GetLength()
+        {
+            return length;
+
+        }
+        public void SetLength(int length)
+        {
+            this.length = length;
+
+        }
+
+        public int GetWidth()
+        {
+            return breadth;
+
+        }
+        public void SetWidth(int breadth)
+        {
+            this.breadth = breadth;
+
+        }
+        public int GetPerimeter()
+        {
+            return (2 * length) + (2 * breadth);
+
+        }
+        public int GetArea()
+        {
+            return length * breadth;
+        }
     }
 }
